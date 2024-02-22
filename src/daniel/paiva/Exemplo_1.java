4b5d9efcbc91070743fc0be5d1899d974d621128
unknown <dpaiva266@hotmail.com>
2024-02-22 20:09:55 -0300

    *Comandos para conssole

---
+++ src/daniel/paiva/Exemplo_1.java
@@ -0,0 +1,15 @@
+package daniel.paiva;
+
+import java.util.Scanner;
+
+public class Exemplo_1 {
+    public static void main(String args[]){
+
+        Scanner s = new Scanner(System.in);
+        System.out.println("digite sua idade:");
+        int idade = s .nextInt();
+        System.out.println("Voce ten  " +idade+ "  anos");
+
+
+    }
+}
